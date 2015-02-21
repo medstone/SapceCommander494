9c1bc0a4b0a7189dcdd1ea30baaf88d2872c9a17
Christopher Jenkins <cwjenk@gmail.com>
2015-02-21 01:34:07 -0500

    pew pew pew (is a thing)

--- Assets/__Scripts/PlayerControl.cs
+++ Assets/__Scripts/PlayerControl.cs
@@ -6,22 +6,27 @@ public class PlayerControl : MonoBehaviour {
 
 	InputDevice theInputDevice; 
 	
-	//GameObject childSphere;
+	public GameObject childSphere;
 	
 	public float rotationAmount;
 	public float moveSpeed;
-	
-	
-	public bool triggerDown;
+
+	public GameObject projectilePrefab;
+	GameObject projectile;
+
+	public float rateOfFire;
+	bool canShoot;
+	bool triggerDown;
 	
 	void Awake(){
-		//childSphere = GameObject.Find ("lasergun");
+
 	}
 	
 	// Use this for initialization
 	void Start () {
 		theInputDevice = InputManager.ActiveDevice;
 		triggerDown = false;
+		canShoot = true;
 	}
 	
 	// Update is called once per frame
@@ -35,14 +40,32 @@ public class PlayerControl : MonoBehaviour {
 		float xIn = theInputDevice.RightStickX;
 		
 		triggerDown = theInputDevice.RightTrigger;
+		if (triggerDown && canShoot)
+			StartCoroutine (ShotTimer ());
 		
 		if (xIn == 0f && yIn == 0f)
 			return;
 		float angle = Mathf.Atan2 (yIn, xIn) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.AngleAxis (90.0f - angle, Vector3.up), Time.deltaTime * rotationAmount);
-		
-		
-		
-		
+
+
+	}
+
+	IEnumerator ShotTimer(){
+		float startTime = Time.time;
+		canShoot = false;
+		// make a projectile 
+		projectile = Instantiate (projectilePrefab) as GameObject;
+		Projectile pro = projectile.GetComponent<Projectile> ();
+		pro.transform.position = childSphere.transform.position;
+		pro.transform.rotation = childSphere.transform.rotation;
+		pro.bearing = childSphere.transform.position - transform.position;
+		pro.bearing.Normalize ();
+		// give it a bearing as is appropriate for the way the weapon is facing.
+
+		while (Time.time - startTime < rateOfFire) {
+			yield return null;
+		}
+		canShoot = true;
 	}
 }
---
+++ Assets/__Scripts/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// projectiles should destroy themselves when they collide with *something* be it a wall or a player
+public class Projectile : MonoBehaviour {
+
+	public float speed;
+	public Vector3 bearing;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	void FixedUpdate () {
+		float dt = Time.fixedDeltaTime;
+		Vector3 pos = transform.position;
+		pos.x += dt * speed * bearing.x;
+		pos.z += dt * speed * bearing.z;
+		transform.position = pos;
+	}
+
+	void OnTriggerEnter(Collider coll){
+		// this could possibly be bad if bullets could ever spawn inside of some collider. 
+	}
+
+	void OnTriggerStay(Collider coll){
+		if (coll.tag == "Player") {
+			//get player component, .TakeHit();
+		}
+	}
+	
+}
