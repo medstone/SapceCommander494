af85dd2b443dcbbc0f114ae20be5fa88618b0799
Eric Taseski <arvic22@gmail.com>
2015-02-22 19:04:17 -0500

    Adding in the main level
    Please don't conflict bro

---
+++ Assets/__Scripts/SlidingDoor.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlidingDoor : MonoBehaviour {
+	
+	Transform open;
+	Transform close;
+	Transform door;
+	
+	public float doorSpeed = 1f;
+	public float margin = .1f;
+	public bool isClosing = false;
+	
+	// Use this for initialization
+	void Awake () {
+		open = transform.Find("OpenPos");
+		close = transform.Find("ClosedPos");
+		door = transform.Find("Door");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider coll) {
+		if(coll.tag != "Actor") return;
+		
+		StartCoroutine(OpenDoor());
+	}
+	
+	IEnumerator OpenDoor () {
+		collider.enabled = false;
+		isClosing = false;
+		
+		while (true) {
+			
+			// Move door towards open position
+			float step = doorSpeed * Time.deltaTime;
+			door.position = Vector3.MoveTowards(door.position, open.position, step);
+			
+			// Check if door is sufficiently close enough to open position
+			float diff = Mathf.Abs((door.position - open.position).magnitude);
+			if(diff < margin) {
+				break;
+			}
+			
+			yield return null;
+		}
+		
+		yield return new WaitForSeconds(2f);
+		StartCoroutine(CloseDoor());
+	}
+	
+	IEnumerator CloseDoor () {
+		
+		collider.enabled = true;
+		isClosing = true;
+		while (isClosing) {
+			
+			// Move door towards open position
+			float step = doorSpeed * Time.deltaTime;
+			door.position = Vector3.MoveTowards(door.position, close.position, step);
+			
+			// Check if door is sufficiently close enough to close position
+			float diff = Mathf.Abs((door.position - close.position).magnitude);
+			if(diff < margin) {
+				isClosing = false;
+				break;
+			}
+			
+			yield return null;
+		}
+		
+	}
+}
