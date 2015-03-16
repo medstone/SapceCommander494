9c709190be19cf74015e5e8c3947e682a9f77ac6
Eric Taseski <arvicz22@gmail.com>
2015-03-16 00:52:06 -0400

    Added player UI that renders to their camera
    Has health bar and text stating which player they are.

---
+++ Assets/__Scripts/PlayerUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerUI : MonoBehaviour {
+	
+	Transform healthBar;
+	float healthBarMaxWidth;
+	
+	PlayerStats stats;
+	
+	// Use this for initialization
+	void Start () {
+		// grab the stats 
+		stats = GetComponentInParent<FollowObject>().target.GetComponent<PlayerStats>();
+		if(!stats) {
+			print("PlayerUI: Unable to get player stats");
+		}
+		
+		healthBar = transform.Find("HealthBar");
+		if(!stats) {
+			print("PlayerUI: Unable to healthbar child");
+		}
+		else {
+			healthBarMaxWidth = healthBar.localScale.x;
+		}
+	}
+	
+	void FixedUpdate () {
+		// update health bar
+		float percentHealthLeft = (float) stats.health / stats.startingHealth;
+		Vector3 sz = healthBar.localScale;
+		sz.x = healthBarMaxWidth * percentHealthLeft;
+		healthBar.localScale = sz;
+	}
+}
