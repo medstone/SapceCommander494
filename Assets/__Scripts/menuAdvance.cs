a461e5a5ba8d7d67cd6e67a11a3f8e719dbe352f
medstone <medstone@umich.edu>
2015-04-01 11:48:47 -0400

    Menu and Script
    Final push of the tutorial changes. Just the menu scenes and script to
    manage them.

---
+++ Assets/__Scripts/menuAdvance.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class menuAdvance : MonoBehaviour {
+	public bool check = false;
+	public string nextScene = "";
+	//public InputManager man;
+
+	// Use this for initialization
+	void Start () {
+		check = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0;i < InputManager.Devices.Count && !check;++i){
+			if(InputManager.Devices[i].AnyButton){
+				print("Fail");
+				return;
+			}
+		}
+		check = true;
+		for (int i = 0;i < InputManager.Devices.Count && check;++i){
+			if(InputManager.Devices[i].AnyButton){
+				print("Sucess");
+				Application.LoadLevel(nextScene);
+			}
+		}
+	}
+}
