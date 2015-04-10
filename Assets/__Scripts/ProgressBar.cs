2852916a02ef52d6d10a591389918c0a55dc86d3
Eric Taseski <arvicz22@gmail.com>
2015-04-09 20:11:08 -0400

    tm 19 add timer to spaceship indicator
    Updated the matchUI prefab. Started my own scene off Chris3.1

--- Assets/__Scripts/ProgressBar.cs
+++ Assets/__Scripts/ProgressBar.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ProgressBar : MonoBehaviour {
@@ -13,6 +14,7 @@ public class ProgressBar : MonoBehaviour {
 	Transform endPos;
 	Transform rocket;
 	Vector3 travelVec = Vector3.zero;
+	Text durationText;
 	
 	
 	// Use this for initialization
@@ -20,6 +22,7 @@ public class ProgressBar : MonoBehaviour {
 		startPos = transform.Find("Start");
 		endPos = transform.Find("End");
 		rocket = transform.Find("Rocket");
+		durationText = transform.GetComponentInChildren<Text>();
 	}
 	
 	void Start () {
@@ -39,6 +42,10 @@ public class ProgressBar : MonoBehaviour {
 		if(isRunning){
 			// Update the time
 			currentTime += Time.deltaTime;
+			float timeLeft = matchTime - currentTime;
+			string mins = Mathf.Floor(timeLeft / 60).ToString();
+ 			string secs = Mathf.Floor(timeLeft % 60).ToString();
+ 			durationText.text = mins + ":" + secs;
 		
 			// determine new position for the rocket
 			float completed = currentTime / matchTime;
