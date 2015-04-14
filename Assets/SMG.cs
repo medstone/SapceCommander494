6d1209280920791d5fefad2b064f11703eb06f85
rreneker <rreneker@umich.edu>
2015-04-13 20:12:17 -0400

    SMG complete

---
+++ Assets/SMG.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SMG : Weapon {
+
+	bool delaying = false;
+
+	// Use this for initialization
+	protected override void Start () {
+		damage = 1;
+		ammunition = startingAmmo;
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (ammunition <= 0 && !delaying) {
+			StartCoroutine(DestroyWeaponDelay());
+		}
+	}
+
+	IEnumerator DestroyWeaponDelay(){
+		delaying = true;
+		yield return new WaitForSeconds (0.25f);
+		PlayerStats playerRef = GetComponentInParent<PlayerStats>();
+		playerRef.defaultWeapon.enabled = true;
+		playerRef.secondaryWeapon = null;
+		Destroy (this.gameObject);
+	}
+
+	protected override void ShotBehavior(){
+		// make three projectiles
+		// first projectile (straight) is just base class shot
+		base.ShotBehavior ();
+
+		--ammunition;
+	}
+}
