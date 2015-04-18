6a2df9c158e2de342b4eb0e2c9494f9aa77fedac
Eric Taseski <arvic22@gmail.com>
2015-04-18 19:31:23 -0400

    demo scene loops now

---
+++ Assets/__Scripts/SpaceToPlayMain.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceToPlayMain : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.Space)) {
+			Application.LoadLevel("MainLevel");
+		}
+	}
+}
