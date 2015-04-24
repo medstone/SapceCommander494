7fca71c70896fa7bc82de8ae053d8f10c354cedf
rreneker <rreneker@umich.edu>
2015-04-23 20:18:56 -0400

    lines from barrier room to barrier now have moving stuff

--- Assets/__Scripts/BarrierControlRoom.cs
+++ Assets/__Scripts/BarrierControlRoom.cs
@@ -5,7 +5,15 @@ public class BarrierControlRoom : MonoBehaviour {
 	public Control control;
 	public GameObject[] barriers;
 	public LineRenderer[] lines; // sprites that lead to the barriers controlled by this room
+	public Material line_mat;
+	public bool cop_cont;
 	RoomConsole console;
+	Color blue;
+	Color black;
+	Color red;
+	Color blue_adj;
+	Color black_adj;
+	float timer = 0.0f;
 
 	public bool Broken(){
 		if (console == null)
@@ -16,17 +24,43 @@ public class BarrierControlRoom : MonoBehaviour {
 	void Awake(){
 		control = GetComponent<Control> ();
 		console = GetComponentInChildren<RoomConsole> ();
+		blue = new Color (0f, 0f, 1f, 1f);
+		black = new Color (0f, 0f, 0f, 1f);
+		red = new Color (0.5f, 0f, 0f, 1f);
+		blue_adj = blue;
+		black_adj = black;
+
 	}
 
 
 	void Start(){
 		control.CapturedEvent += OnCapture;
 		foreach (LineRenderer line in lines) {
-			if (control.holds == Faction_e.spaceCop)
-				line.material = control.copColor;
-			else
-				line.material = control.crimColor;
+			line.material = line_mat;
+			if (control.holds == Faction_e.spaceCop){
+				line.SetColors(blue,black);
+				cop_cont = true;
+			}
+			else{
+				line.SetColors(red,red);
+				cop_cont = false;
+			}
 		}
+	}
+
+	void FixedUpdate(){
+		if (cop_cont == true) {
+			foreach (LineRenderer line in lines) {
+				timer += Time.deltaTime;
+				float blue_a = Mathf.Cos (3 * timer);
+				float black_a = Mathf.Sin (3 * timer);
+				blue_adj = new Color (0f, 0f, blue_a, 1f);
+				black_adj = new Color (0f, 0f, black_a, 1f);
+				line.SetColors (blue_adj, black_adj);
+				
+			}
+		}
+
 	}
 	
 
@@ -35,10 +69,16 @@ public class BarrierControlRoom : MonoBehaviour {
 			Destroy(go.gameObject); // should it actually destroy it?
 		}
 		foreach(LineRenderer line in lines){
-			if (new_team == Faction_e.spaceCrim)
-				line.material = control.crimColor;
-			else
-				line.material = control.copColor;
+			if (new_team == Faction_e.spaceCrim){
+				line.SetColors(red,red);
+				cop_cont = false;
+			}
+			else{
+				line.SetColors(blue,black);
+				blue_adj = blue;
+				black_adj = black;
+				cop_cont = true;
+			}
 		}
 		MatchManager.S.KeyRoomCaptured (); // notify match manager that this key room was captured.
 	}
---
+++ Assets/line_flash.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class line_flash : MonoBehaviour {
+
+	Color start;
+	Color black;
+	LineRenderer line;
+	float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		line = this.gameObject.GetComponent<LineRenderer> ();
+		start = new Color (1f, 0f, 0f, 1f);
+		black = new Color (0f, 0f, 0f, 1f);
+		line.SetColors (start, black);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		float start_shade = Mathf.Cos (5*timer);
+		float black_shade = Mathf.Sin (5*timer);
+		start = new Color (start_shade, 0f, 0f, 1f);
+		black = new Color (black_shade, 0f, 0f, 1f);
+		line.SetColors (start, black);
+	}
+}
